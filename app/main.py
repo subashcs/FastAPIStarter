f931c63a1353ac8768fda6784b08bd2f6ba84524
Subash Chandra Sapkota <scs@logpoint.com>
2023-07-13 22:00:29 +0545

    chore: add swagger doc config
    docs are available at /docs api route

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
 from .user_router import router as user_router
 
 app = FastAPI()
@@ -7,4 +8,13 @@ app = FastAPI()
 async def read_main():
     return {"msg": "Hello World"}
 
+
 app.include_router(user_router)
+
+def openapi_html():
+    return get_swagger_ui_html(
+        openapi_url="/openapi.json",
+        title="FastAPI Swagger UI"
+    )
+
+app.add_route("/docs", openapi_html, name="swagger_ui")
